294f1a8bbd95c8429cf616e5e37251605b458e2c
LucasNiel <lucas_nielsen1@hotmail.com>
2015-09-09 08:58:59 +1000

    Search form modified and Functionality Improved
    All CSS incorporated into both pages with functionality improved
    including link to page.php and search form on search1.php

--- page.php
+++ page.php
@@ -1,40 +1,55 @@
-<html>
+<!--Created and Modified By Lucas and Michael.
+Michael: Modification of the forms and tables with controlled CSS
+Lucas: Created all forms, tables and in general foundation of page with html, PHP and MySql
+    -->
+<html lang="en">
 
 <head>
-
-<title>Property Data</title>
-
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="css/theme.css" rel="stylesheet">
+	<title>Property Data</title>
+	<link rel='stylesheet' href = 'css/page.css' type = 'text/css'/>
 </head>
 
 <body>
+<div class="page-header">
+	<h1>Property View</h1>
+</div>
+	<?php
+		include('includes/content/topNav.php');
+		include('includes/functions/db.php');
 
-<?php
-include('includes/content/topNav.php');
-include ('/includes/functions/db.php');
-$pdo=db_connect();
+		$pdo=db_connect();
 
-$id=(isset($_GET["data"])) ? (int)$_GET["data"] : 1;
+		$id=(isset($_GET["data"])) ? (int)$_GET["data"] : 1;
 
-$stmt = $pdo->prepare(
-						"SELECT * FROM v_w_house_property_details WHERE propertyId = '.$id.'");					
+		$stmt = $pdo->prepare(
+								'SELECT * FROM v_w_house_property_details '.
+								'WHERE propertyId = '. $id
+								);					
 		
-	$stmt->execute();
+			$stmt->execute();
+				
+			$result = $stmt->fetch();
+    echo '<div id=content>';
 		
-		
-	$result = $stmt->fetch(PDO::FETCH_OBJ);
-	
+		echo '<div id=Address>'.$result["Address"].'</div>';
+		echo "<div id=Picture>Insert Picture Here</div>";
+		echo '<div id=RoomNum>Number of rooms: '. $result["NumberofRooms"].'</div>';
+		echo '<div id=Price>Buying Price: $'. $result["BuyingPrice"].'</div>';
+		echo '<div id=Description>Description: '.$result["Description"].'</div>';
+
+	echo '</div>';
+	?>
 
 
 
-?>
-<table>
-<?php 
-echo("<tr><td>Picture</td></tr><tr><td>$result->Address</td></tr><tr><td>$result->NumberofRooms</td></tr><tr><td>$result->BuyingPrice</td></tr><tr><td>$result->Description</td></tr>"); 
-
-
-?>
-</table>
 
 </body>
-
 </html>
+
--- search1.php
+++ search1.php
@@ -1,97 +1,93 @@
-
-<html>
-<title>Search data</title>
+<!--Created and Modified By Lucas and Michael.
+Michael: Modification of the forms and tables with controlled CSS
+Lucas: Created all forms, tables and in general foundation of page with html, PHP and MySql
+    -->
+<html lang="en">
 <head>
-<link rel='stylesheet' href = 'css/global.css' type = 'text/css'/>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="css/theme.css" rel="stylesheet">
+	<title>Search data</title>
+	<link rel='stylesheet' href = 'css/search.css' type = 'text/css'/>
 </head>
 
 <body>
 <?php include('/includes/content/topNav.php'); ?>
-
-<form method="post" action="search1.php">
+<div class="page-header">
+<h1>Property Search</h1>
+<form class="searchBox" method="post" action="search1.php">
 <fieldset>
-<?php 
-include ('includes/functions/db.php');
-include ('includes/functions/formControls.php');
+	<?php 
+		include ('includes/functions/db.php');
+		include ('includes/functions/formControls.php');
 
-$wHouseDetails = db_getWHouseDetails($propertyId);
+		$wHouseDetails = db_getWHouseDetails($propertyId);
 
-ctrl_input_field($errors,'text','REQUIRED','address','Address<br>','txtAddress',$wHouseDetails['address']);
+		ctrl_input_field($errors,'text','REQUIRED','searchinput','Address    ','txtAddress',$wHouseDetails['address']);
 
-
-	?>		
 		
-		<table>
-			<tr><td>Property Type</td><td>Min Bed</td><td>Max Bed</td><td>Min Price</td><td>Max Price</td></tr>
-		</table>
-		<?php
-		$value_propertytype = array('Any', 'WholeHouse','ShareHouse');
-		$value_bed = array('Any','1','2','3','4','5');
-		$value_price = array('Any','50000','100000','200000','300000','400000','500000','1000000');
+		$value_propertytype = array('Property Type', 'WholeHouse','ShareHouse');
+		$value_bedMin = array('Min Beds','1','2','3','4','5');
+		$value_bedMax = array('Max Beds','1','2','3','4','5');
+		$value_priceMin = array('Min Price','50000','100000','200000','300000','400000','500000','1000000','10000000');
+		$value_priceMax = array('Max Price','50000','100000','200000','300000','400000','500000','1000000','10000000');
 		
 		ctrl_select($errors,'propertyType',$value_propertytype,'Class_1','');
-		ctrl_select($errors,'Min Bed',$value_bed,'Class_2','');
-		ctrl_select($errors,'Max Bed',$value_bed,'Class_3','');
-		ctrl_select($errors,'Min Price',$value_price,'Class_4','');
-		ctrl_select($errors,'Max Price',$value_price,'Class_5','');
+		ctrl_select($errors,'Min Bed',$value_bedMin,'Class_2','');
+		ctrl_select($errors,'Max Bed',$value_bedMax,'Class_3','');
+		ctrl_select($errors,'Min Price',$value_priceMin,'Class_4','');
+		ctrl_select($errors,'Max Price',$value_priceMax,'Class_5','');
 		ctrl_submit('Search','submit');
-		?>
-		
-		
-		
-	</fieldset>
+	?>
+</fieldset>
 </form>
+</div>
 
-<table>
+<div id=searchResults>
+<table class=results>
+   
+	  
+	<?php
+		if(isset($_POST['submit'])){
+			
+		$address=$_POST['searchinput'];
+		$Minrooms=$_POST['Min Bed'];
+		$Maxrooms=$_POST['Max Bed'];
+		$Minprice=$_POST['Min Price'];
+		$Maxprice=$_POST['Max Price'];
 
-  <tr>
-
-    <td align="center">Property Data</td>
-
-  </tr>
-
-  <tr>
-
-    <td>
-
-      <table border="0">      
-<?php
-
-
-if(isset($_POST['submit'])){
-
-
-	
-$address=$_POST['searchinput'];
-$Minrooms=$_POST['Min Bed'];
-$Maxrooms=$_POST['Max Bed'];
-$Minprice=$_POST['Min Price'];
-$Maxprice=$_POST['Max Price'];
-
-$pdo=db_connect();
-$stmt = $pdo->prepare(
-						'SELECT propertyId,NumberofRooms,BuyingPrice,Address FROM v_w_house_property_details '.
-						'WHERE Address LIKE '%" . $address .  "%''
-						);
+		$pdo=db_connect();
+		$stmt = $pdo->prepare(
+								'SELECT propertyId,NumberofRooms,BuyingPrice,Address FROM v_w_house_property_details '.
+								'WHERE Address == '%" . $address .  "%''
+								//'OR NumberofRooms BETWEEN '%" . $Minrooms .  "%' AND '%" . $Maxrooms .  "%''.
+								//'OR BuyingPrice BETWEEN '%" . $Minprice .  "%' AND '%" . $Maxprice .  "%''
+								);
+				
+			$stmt->execute();
+				
+			$result = $stmt->fetchAll();
+		foreach( $result as $row ){
+    
 		
-	$stmt->execute();
+		echo "<tr><td style=\"font-size:40px\">Insert Picture Here</td></tr>";
+		echo '<tr><td style=\"font-size:20px\>'.$row['Address'].'</td></tr>';
+		echo '<tr><td>Number of rooms:'. $row['NumberofRooms'].'</td><td>Buying Price:'. $row['BuyingPrice'].'</td>';
+		echo "<td><a href='page.php?data=".$row['propertyId']."'>More Details</a></td></tr>";
+		echo "<tr><td><div class=\"linediv\"></div></td></tr>";
 		
-	$result = $stmt->fetch(PDO::FETCH_OBJ);
-	echo "<tr><td>Picture</td></tr><tr><td>$result->Address</td></tr>";
-echo "<tr><td>$result->NumberofRooms</td></tr><tr><td>$result->BuyingPrice</td></tr>";
-echo("<tr><td><a href='page.php?data=".$result->propertyId."'>Details</a></td></tr>");
-}
-?>
+		}
+		}
+		
+	?>
 
-
-
-
-    </table>
-
-  </td>
-
-</tr>
 
 </table>
+</div>
 
 </body>
+</html>
